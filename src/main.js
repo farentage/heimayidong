b8ce9a0c5c554226900656482b281395680ba327
unknown <you@example.com>
2020-01-06 10:50:09 +0800

    项目初始化-初始化目录-导入vant

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-Vue.config.productionTip = false
+import './utils/register-vant.js'
 
 new Vue({
   router,
